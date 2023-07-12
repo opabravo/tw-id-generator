e42a0b22fedd3ce881b9052de0a79d6e219e87a8
FateWalker <21262207+opabravo@users.noreply.github.com>
2023-07-12 13:14:18 +0800

    Remove deprecated `language_level ` from setup

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 
-setup(ext_modules = cythonize('utils.pyx'), language_level = "3")
+setup(ext_modules = cythonize('utils.pyx'))
